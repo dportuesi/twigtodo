bc6219a88285a1c5efb75ec5e398569f8a906713
Domenic Portuesi <dportuesi@gmail.com>
2024-10-06 14:19:22 -0400

    Update card UI

--- app/src/main/java/com/branchapp/twigtodo/ui/components/TodoCard.kt
+++ app/src/main/java/com/branchapp/twigtodo/ui/components/TodoCard.kt
@@ -1,8 +1,8 @@
 package com.branchapp.twigtodo.ui.components
 
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
@@ -12,6 +12,7 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.rememberVectorPainter
 import androidx.compose.ui.res.stringResource
@@ -37,14 +38,15 @@ fun TodoCard(
                 .padding(16.dp),
         ) {
             Text(
+                modifier = Modifier
+                    .weight(1f)
+                    .padding(end = 8.dp),
                 style = MaterialTheme.typography.titleLarge,
                 text = title
             )
-            Spacer(
-                modifier = Modifier.weight(1f)
-            )
             Icon(
                 modifier = Modifier
+                    .align(Alignment.CenterVertically)
                     .clickable {
                         onClick()
                     },
@@ -59,9 +61,16 @@ fun TodoCard(
 @Composable
 fun TodoCardPreview() {
     TwigTodoTheme {
-        TodoCard(
-            title = "preview",
-            onClick = { }
-        )
+        Column {
+            TodoCard(
+                title = "preview",
+                onClick = { }
+            )
+            TodoCard(
+                title = "really really long preview really really long preview " +
+                        "really really long preview really really long preview",
+                onClick = { }
+            )
+        }
     }
 }
